d7a919a44c48bb0e7c2ec14f605d1079501b2d44
Marko Lozančić <lozamarko@hotmail.com>
2020-03-27 16:55:49 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
+
+function HatsPage() {
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
